4d8a448dddc7bff6d2677be397e6a0b6cdca5866
victor fonseca <victorfonsecass@outlook.com>
2024-08-21 19:46:37 -0300

    feat(button): add Button component with customizable SVG icon
    Added a new Button component that renders a button element with customizable classes and SVG icon. The button accepts props such as className, href, onClick, children, px, and white. The Button component uses the renderButton function to return the button structure, which includes the provided children and an SVG icon (ButtonSvg), with the option to customize the SVG color based on the white prop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
-
+import Button from './components/Button'
 const App = () => {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button class="mt-10" href="login">
+          something
+        </Button>
+      </div>
       <ButtonGradient />
     </>
   );
