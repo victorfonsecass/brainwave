937f6e177c6768fc1566d91679222c6f01da87ef
victor fonseca <victorfonsecass@outlook.com>
2024-08-21 19:30:51 -0300

    feat: add ButtonGradient component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-function App() {
+import ButtonGradient from './assets/svg/ButtonGradient'
 
+const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App
